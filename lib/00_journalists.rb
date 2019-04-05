3cd4c243f3df42a6ff2430847e8798d9b7bd57db
JP MAGIDO <jpmagido@me.com>
2019-04-05 19:24:28 +0200

    EXO1 FINI

---
+++ lib/00_journalists.rb
@@ -0,0 +1,322 @@
+# DEBUT LISTE----------------------------------------------------
+
+a = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier",
+	"@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle",
+	"@julienduffe","@thomaspoupeau","@LilyRossignol",
+	"@ClairGuedon","@stephanieauguy","@claw_prolongeau",
+	"@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet",
+	"@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy",
+	"@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon",
+	"@HeloAb","@Panamorama","@gregplou","@BenoitBerthe",
+	"@LauraBruneau89","@Anthony_Lieures","@Sharonwaj",
+	"@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION",
+	"@SophiFay","@bdelombre","@annalecerf","@AdriaBudry",
+	"@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon",
+	"@e_vallerey","@IsabelleSouquet","@AudeDavidRossi",
+	"@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate",
+	"@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier",
+	"@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven",
+	"@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut",
+	"@cyceron","@gregory_raymond","@vhunsinger","@l_peillon",
+	"@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry",
+	"@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy",
+	"@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud",
+	"@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu",
+	"@VirginK","@b_misa","@FabriceCouste","@barbara_prose",
+	"@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox",
+	"@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc",
+	"@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch",
+	"@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur",
+	"@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit",
+	"@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne",
+	"@bobdobolino","@murdever","@YGrandmontagne","@Mnyo",
+	"@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom",
+	"@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux",
+	"@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli",
+	"@ChloeAeberhardt","@MartineJacot","@JuliaPascualita",
+	"@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau",
+	"@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas",
+	"@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt",
+	"@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx",
+	"@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli",
+	"@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr",
+	"@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon",
+	"@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy",
+	"@PlummerWilliam", "@OlivierMarin1","@alaurefremont",
+	"@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie",
+	"@Lnpagesy","@marclandre","@paoliniesther","@Feertchak",
+	"@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM",
+	"@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray",
+	"@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375",
+	"@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre",
+	"@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD",
+	"@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_",
+	"@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti",
+	"@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier",
+	"@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri",
+	"@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna",
+	"@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen",
+	"@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot",
+	"@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour",
+	"@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean",
+	"@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer",
+	"@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez",
+	"@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano",
+	"@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1",
+	"@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon",
+	"@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch",
+	"@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM",
+	"@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli",
+	"@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann",
+	"@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix",
+	"@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva",
+	"@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59",
+	"@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir",
+	"@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet",
+	"@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere",
+	"@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver",
+	"@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM",
+	"@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche",
+	"@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot",
+	"@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer",
+	"@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums",
+	"@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland",
+	"@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_",
+	"@NicoRichaud","@RHoueix","@simottel","@DamienLicata",
+	"@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben",
+	"@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran",
+	"@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt",
+	"@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin",
+	"@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades",
+	"@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji",
+	"@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon",
+	"@Allocab","@epenser","@JAGventeprivee","@frwrds",
+	"@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain",
+	"@LoriHelloc"]
+
+# FIN LISTE----------------------------------------------------
+
+# NOMBRE DE JOURNALISTES
+## -----------------------------------------------------------
+
+nbr_j = a.size
+
+puts "
+-----
+Il y a #{nbr_j} journalistes dans la liste
+-----
+
+********"
+
+## FIN NOMBRE DE JOURNALISTES --------------------------------
+
+
+# NOMBRE DANS ARRAY
+## -----------------------------------------------------------
+
+
+
+nbr_nbr = [] << (a.select {|s| s.include? '1'} + 
+a.select {|s| s.include? '2'} +
+a.select {|s| s.include? '3'} +
+a.select {|s| s.include? '4'} +
+a.select {|s| s.include? '5'} +
+a.select {|s| s.include? '6'} +
+a.select {|s| s.include? '7'} +
+a.select {|s| s.include? '8'} +
+a.select {|s| s.include? '9'} +
+a.select {|s| s.include? '0'})
+
+
+puts "
+-----
+L'Array contient #{nbr_nbr[0].size} éléments avec des chiffres.
+-----
+
+********"
+
+## FIN NOMBRE DANS ARRAY-------------------------------------
+
+# AUDE DANS ARRAY
+##-----------------------------------------------------------
+nom = []
+nom << a.select {|s| s.include? 'aude'}
+puts "
+-----
+#{nom[0].size} fichier(s) contient les lettres 'aude'. 
+-----
+
+********"
+
+## FIN AUDE DANS ARRAY-----------------------------------------
+
+# DEBUT EN MAJ DANS ARRAY
+##-----------------------------------------------------------
+maj = []
+maj << (a.select{|x|x[/^@A/]}+
+	a.select{|x|x[/^@B/]}+
+	a.select{|x|x[/^@C/]}+
+	a.select{|x|x[/^@D/]}+
+	a.select{|x|x[/^@E/]}+
+	a.select{|x|x[/^@F/]}+
+	a.select{|x|x[/^@G/]}+
+	a.select{|x|x[/^@H/]}+
+	a.select{|x|x[/^@I/]}+
+	a.select{|x|x[/^@J/]}+
+	a.select{|x|x[/^@K/]}+
+	a.select{|x|x[/^@L/]}+
+	a.select{|x|x[/^@M/]}+
+	a.select{|x|x[/^@N/]}+
+	a.select{|x|x[/^@O/]}+
+	a.select{|x|x[/^@P/]}+
+	a.select{|x|x[/^@Q/]}+
+	a.select{|x|x[/^@R/]}+
+	a.select{|x|x[/^@S/]}+
+	a.select{|x|x[/^@T/]}+
+	a.select{|x|x[/^@U/]}+
+	a.select{|x|x[/^@V/]}+
+	a.select{|x|x[/^@W/]}+
+	a.select{|x|x[/^@X/]}+
+	a.select{|x|x[/^@Y/]}+
+	a.select{|x|x[/^@Z/]})
+
+
+
+puts "
+-----
+L'Array contient #{maj[0].size} éléments avec des Majuscules au début.
+-----
+
+********"
+## FIN MAJ DANS ARRAY ------------------------------------------
+
+
+
+# MAJ DANS ARRAY
+##-----------------------------------------------------------
+
+maj1 = []
+maj1 << (a.select{|s| s.include? 'A'} +
+	a.select{|s| s.include? 'B'}+
+	a.select{|s| s.include? 'C'}+
+	a.select{|s| s.include? 'D'}+
+	a.select{|s| s.include? 'E'}+
+	a.select{|s| s.include? 'F'}+
+	a.select{|s| s.include? 'G'}+
+	a.select{|s| s.include? 'H'}+
+	a.select{|s| s.include? 'I'}+
+	a.select{|s| s.include? 'J'}+
+	a.select{|s| s.include? 'K'}+
+	a.select{|s| s.include? 'L'}+
+	a.select{|s| s.include? 'M'}+
+	a.select{|s| s.include? 'N'}+
+	a.select{|s| s.include? 'O'}+
+	a.select{|s| s.include? 'P'}+
+	a.select{|s| s.include? 'Q'}+
+	a.select{|s| s.include? 'R'}+
+	a.select{|s| s.include? 'S'}+
+	a.select{|s| s.include? 'T'}+
+	a.select{|s| s.include? 'U'}+
+	a.select{|s| s.include? 'V'}+
+	a.select{|s| s.include? 'W'}+
+	a.select{|s| s.include? 'X'}+
+	a.select{|s| s.include? 'Y'}+
+	a.select{|s| s.include? 'Z'})
+
+
+
+puts "
+-----
+L'Array contient #{maj1[0].size} éléments avec des Majuscules.
+-----
+
+********"
+
+##-----------------------------------------------------------
+
+
+##-----------------------------------------------------------
+
+# "_" DANS ARRAY
+# 
+
+maj2 = []
+maj2 << a.select{|s| s.include? '_'}
+
+puts "
+-----
+L'Array contient #{maj2[0].size} éléments avec des _
+-----
+
+********"
+
+## DEBUT ORDRE ALPHABETIQUE ----------------------------------
+puts "
+-----
+Voici une liste alphabétique des journalistes
+-----
+
+"
+
+alpha = []
+alpha << a.sort_by(&:downcase)
+
+puts alpha.inspect
+
+puts "
+
+********
+
+"
+## FIN ORDRE ALPHABETIQUE ----------------------------------
+
+
+
+## DEBUT LENGTH  --------------------------------------------
+
+
+taille = []
+taille << a.sort_by(&:length)
+
+
+
+puts " 
+
+-----
+Voici les 50 éléments les plus courts :
+-----
+"
+
+puts taille[0][1...50].inspect
+
+puts "
+
+********
+"
+
+## FIN LENGTH  -----------------------------------------------
+
+## DEBUT POSITION  ----------------------------------------
+
+posi = []
+posi << a.find_index("@epenser")
+
+puts " 
+
+-----
+@epenser est à la #{posi[0].inspect}e position.
+-----
+
+********"
+
+
+## FIN POSITION  ------------------------------------------
+
+
+
+
+
+
+
+
+
